ea3e6637afcea57cdd2be0008e35170cfe0533ca
michalor <brouzdal91@gmail.com>
2025-11-11 14:02:24 +0000

    Fix structure issues.

--- custom_components/chytrejsiobec/sensor.py
+++ custom_components/chytrejsiobec/sensor.py
@@ -22,7 +22,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from ... import ChytrejsiObecDataUpdateCoordinator
+from . import ChytrejsiObecDataUpdateCoordinator
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
